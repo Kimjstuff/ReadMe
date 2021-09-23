abc6294eab7ba5d430ffad27f57f120542367380
Kimjstuff <kimjstuff@gmail.com>
2021-09-23 19:14:32 +0200

    W38 - Tasks

---
+++ W38/TaskTwo.FunctionsReturnTypes.java
@@ -0,0 +1,101 @@
+//2.a Look at the file TaskTwoA and fill out the missing line, using the happy boolean.
+
+//Task Two A (functions return types)
+/*
+class Main{
+  public static boolean happy = true;
+
+public static void main(String [] args) {
+   if (iAmHappy())
+   {
+     System.out.println("I clap my hands");
+   }
+   else
+   {
+     System.out.println("I don't clap my hands");
+   }
+}
+
+
+public static boolean iAmHappy()
+{
+  // fill out what is missing here:
+  return happy;
+}
+}
+*/
+
+/*
+
+
+//2.b Write a function that receives to integers as parameters and returns the sum of them.
+
+//Task Two B (functions return types)
+class main{
+
+public static void main (String [] args) {
+   int x = 8;
+   int y = 7;
+
+System.out.println (sum(x, y));
+}
+
+static int sum(int x, int y){
+int sum = x + y;
+return sum;
+}
+
+
+}
+*/
+
+/*
+
+
+//2.c Write a function that receives a string and returns it as uppercase. (Hint: ".toUpperCase()". Further hint: https://www.w3schools.com/jsref/jsref_toUpperCase.asp )
+
+//Task Two C (Functions return types)
+
+class Main {
+
+public static void main(String [] args) {
+
+  String str = "Hello World!";
+
+  System.out.println (HelloWorld(str));
+}
+
+
+public static String HelloWorld (String str){
+  str = str.toUpperCase(); // Returns "HELLO WORLD!"
+  return str;
+}
+}
+*/
+
+/*
+
+2.d Write a function that receives a string and returns true if the first letter of the string is uppercase. (Hints: ".charAt(0)" and "Character.isUpperCase('a');" )
+
+//Task Two D (Funtions return types)
+
+class Main {
+
+  public static void main(String [] args) {
+
+  String str = "Hello World!";
+  System.out.println (HelloWorld(str));
+  }
+
+  public static boolean HelloWorld (String str){
+
+//return Character.isUpperCase(str.charAt(0)) - Alternativ løsning til nedenstående
+  if (Character.isUpperCase(str.charAt(0))){
+  return true;
+  }
+  else {
+    return false;
+  }
+}
+}
+*/
