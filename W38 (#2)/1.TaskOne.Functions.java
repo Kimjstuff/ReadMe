32eb4b1c63545bf41fedf9fd5209c9d54b9da540
Kimjstuff <kimjstuff@gmail.com>
2022-05-19 08:53:33 +0200

    W38

---
+++ W38 (#2)/1.TaskOne.Functions.java
@@ -0,0 +1,57 @@
+//1.a Create a file and name it Main.java, include a main method.
+//Task One A (Functions)
+/*
+class Main{
+
+public static void main(String[] args) {
+
+System.out.println("Main_A");
+
+}
+}
+*/
+
+//1.b Write a function that prints an empty line and call it from main();
+//Task One B (Functions)
+/*
+class Main_b{
+
+public static void main(String[] args) {
+
+System.out.println("");
+
+}
+}
+*/
+
+
+//1.c Write a function that receives a string as a parameter and prints it. call this function from main()
+//Task One C (Functions)
+/*
+class Main_c{
+
+public static void main(String[] args) {
+
+String c = "Main_C";
+
+System.out.println(c);
+
+}
+}
+*/
+
+//1.d Write a function that receives a string called "name" and an integer called "age" and call it from main with your own name and age. Have the function print the text "My name is \<name\>, I am <age> years old".
+//Task One D (Functions)
+/*
+class Main_d{
+
+public static void main(String[] args) {
+
+String name = "John";
+int age = 27;
+
+System.out.println("My name is "+(name)+" , I am "+(age)+" years old.");
+
+}
+}
+*/
---
+++ W38 (#2)/3.2.TaskThree.ObjectsStudent).java
@@ -0,0 +1,16 @@
+class Student {
+
+  String name;
+  int age;
+  boolean isFemale = true;
+  String datamatikerTeam;
+
+
+public Student(String tmpName, int tmpAge, boolean tmpIsFemale, String tmpDatamatikerTeam){
+  this.name = tmpName;
+  this.age = tmpAge;
+  this.isFemale = tmpIsFemale;
+  this.datamatikerTeam = tmpDatamatikerTeam;
+
+}
+}//Student class
